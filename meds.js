43cc54de13dcc569692d0bb81b5130c673677eeb
Yohan <yohanpark@medibloc.org>
2018-10-22 17:36:11 +0900

    handTx

--- meds.js
+++ meds.js
@@ -4,22 +4,64 @@ web3.setProvider(provider);
 web3.eth.defaultAccount = web3.eth.accounts[0];
 var stop = false;
 
-var startBlockNo = web3.eth.blockNumber - 20;
+var ABI = [{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":false,"inputs":[{"name":"hashData","type":"string"}],"name":"putPhrHash","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"},{"name":"tokenDecimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"phrHashCount","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"phrHashes","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]
+var MedS = web3.eth.contract(ABI).at("0x40af181010193fbe1516877563c40cc88c72d95a");
+
+/*** 병원 ***/
+function handData() {
+    $('#data_pt').val($('#data_hp').val());
+}
+
+/*** 환자 ***/
+function hashing() {
+    var data = $('#data_pt').val();
+    if(!data) {
+        alert("data가 없습니다.");
+        return;
+    }
+    var hash = web3.sha3(data);
+    $('#hash_pt').val(hash);
+}
+
+function readTxInfoFromBlockchain(txHash) {
+    var txInfo = web3.eth.getTransaction(txHash);
+    if(txInfo.blockNumber)
+        $('#tx_pt').val(txInfo.blockNumber + ":" + txInfo.transactionIndex + ":" + txHash);
+    else
+        setTimeout(function () {
+            readTxInfoFromBlockchain(txHash);
+        }, 1000)
+}
+
+function wbcPhrHash() {
+    var hashData = $('#hash_pt').val().substring(0,32);
+    var txHash = MedS.putPhrHash(hashData);
+    readTxInfoFromBlockchain(txHash);
+}
+
+function handTx() {
+    $('#tx_hp').val($('#tx_pt').val());
+}
+
+/*** 연구소 ***/
+
+
+/*** Monitor ***/
+var startBlockNo = web3.eth.blockNumber - 5; //5건 전 블록부터 참조
 monitorBlockNo = Math.max(0, startBlockNo);
 
 // 감시 개시
 function startMonitor() {
     stop = false;
-    //20건 전 블록부터 참조
     var table = document.getElementById('tbMonitor');
-    for (; monitorBlockNo < web3.eth.blockNumber; monitorBlockNo++) {
+    for (; monitorBlockNo <= web3.eth.blockNumber; monitorBlockNo++) {
         var result = web3.eth.getBlock(monitorBlockNo);
         //console.log(result);
         insertBlockRow(result, table, monitorBlockNo);
     }
     setTimeout(function() {
         watchBlock(table, monitorBlockNo);
-    }, 10000);
+    }, 1000);
 }
 
 // 블록 감시
@@ -27,7 +69,7 @@ function watchBlock(table, blockNumber) {
     if (stop) {
         return;
     }
-    if (blockNumber == parseInt(web3.eth.blockNumber)) {
+    if (blockNumber > parseInt(web3.eth.blockNumber)) {
         setTimeout(function() {
             watchBlock(table, blockNumber);
         }, 1000);
@@ -69,3 +111,5 @@ function insertTranRow(transactions, td) {
 function stopWatch() {
     stop = true;
 }
+
+setTimeout(startMonitor, 500);
--- meds.sol
+++ meds.sol
@@ -1,8 +1,68 @@
 pragma solidity ^0.4.25;
 
-contract MedS {
-    mapping(address => mapping(uint256 => string)) public phrHashes;
-    constructor() public {
+contract BasicToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
 
+    mapping (address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        totalSupply = initialSupply;  // Update total supply with the decimal amount
+        balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
+        name = tokenName;                                   // Set the name for display purposes
+        symbol = tokenSymbol;                               // Set the symbol for display purposes
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
+
+contract MedS is BasicToken{
+    uint8 public decimals;
+
+    mapping(address => uint8) public phrHashCount;
+    mapping(address => mapping(uint256 => string)) public phrHashes;
+
+    constructor(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol,
+        uint8 tokenDecimals
+    ) BasicToken(initialSupply, tokenName, tokenSymbol) public {
+        decimals = tokenDecimals;
+    }
+
+    function putPhrHash(string hashData) public {
+        uint8 index = phrHashCount[msg.sender]; // next index = count
+        phrHashes[msg.sender][index] = hashData;
+        phrHashCount[msg.sender] = index+1;
     }
 }
