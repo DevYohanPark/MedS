094a99d4dff630e8ac04edd98b39828fa4520be6
Yohan <yohanpark@medibloc.org>
2018-10-23 10:47:24 +0900

    수정

--- meds.js
+++ meds.js
@@ -1,13 +1,3 @@
-var web3 = new Web3();
-var provider = new web3.providers.HttpProvider("http://localhost:8545");
-web3.setProvider(provider);
-web3.eth.defaultAccount = web3.eth.accounts[0];
-var stop = false;
-
-var contractAddr = "0xc3032677f47b7135eaba178121406e1de95dc849";
-var ABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"hashData","type":"string"}],"name":"putPhrHash","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"patient","type":"address"},{"name":"phrIndex","type":"uint8"},{"name":"signData","type":"string"}],"name":"putSign","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"phrHashCount","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"phrHashes","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"phrSigns","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"},{"name":"tokenDecimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
-var MedS = web3.eth.contract(ABI).at(contractAddr);
-
 /*** 병원 ***/
 function handData() {
     $('#data_pt').val($('#data_hp').val());
@@ -129,9 +119,6 @@ function sendMedS() {
 }
 
 /*** Monitor ***/
-var startBlockNo = web3.eth.blockNumber - 5; //5건 전 블록부터 참조
-monitorBlockNo = Math.max(0, startBlockNo);
-
 function watchMedS() {
     $('#meds_hp').text(MedS.balanceOf(web3.eth.accounts[1]).c[0]);
     $('#meds_pt').text(MedS.balanceOf(web3.eth.accounts[2]).c[0]);
@@ -142,7 +129,6 @@ function watchMedS() {
 
 // 감시 개시
 function startMonitor() {
-    stop = false;
     var table = document.getElementById('tbMonitor');
     for (; monitorBlockNo <= web3.eth.blockNumber; monitorBlockNo++) {
         var result = web3.eth.getBlock(monitorBlockNo);
@@ -155,9 +141,6 @@ function startMonitor() {
 
 // 블록 감시
 function watchBlock(table, blockNumber) {
-    if (stop) {
-        return;
-    }
     if (blockNumber > parseInt(web3.eth.blockNumber)) {
         setTimeout(function() {
             watchBlock(table, blockNumber);
@@ -196,10 +179,28 @@ function insertTranRow(transactions, td) {
     td.innerHTML = "<input type='text' value='" + transactions.length + "개:" + allData + "' /></td>";
 }
 
-// 정지
-function stopWatch() {
-    stop = true;
-}
+/*** 전역변수 ***/
+var web3 = null;
+var contractAddr = 0;
+var MedS = null;
 
-setTimeout(startMonitor, 500);
-setTimeout(watchMedS, 1000);
+var startBlockNo = 0;
+
+$().ready(function() {
+    web3 = new Web3();
+    var provider = new web3.providers.HttpProvider("http://localhost:8545");
+    web3.setProvider(provider);
+    web3.eth.defaultAccount = web3.eth.accounts[0];
+
+    contractAddr = $('#contractAddr').val();
+    var ABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"hashData","type":"string"}],"name":"putPhrHash","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"patient","type":"address"},{"name":"phrIndex","type":"uint8"},{"name":"signData","type":"string"}],"name":"putSign","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"phrHashCount","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"phrHashes","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"phrSigns","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"initialSupply","type":"uint256"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"},{"name":"tokenDecimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
+    MedS = web3.eth.contract(ABI).at(contractAddr);
+
+    setTimeout(watchMedS, 1000);
+
+    startBlockNo = web3.eth.blockNumber - 5; //5건 전 블록부터 참조
+    monitorBlockNo = Math.max(0, startBlockNo);
+
+    setTimeout(startMonitor, 500);
+});
+
